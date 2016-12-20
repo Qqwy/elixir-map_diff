92552b4f4fe18d0a96b69d873e9a0e2e1f2d7486
Qqwy/Wiebe-Marten <qqwy@gmx.com>
2016-12-20 18:17:19 +0100

    bump version number.

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule MapDiff.Mixfile do
 
   def project do
     [app: :map_diff,
-     version: "1.0.1",
+     version: "1.1.0",
      # build_path: "../../_build",
      # config_path: "../../config/config.exs",
      # deps_path: "../../deps",
